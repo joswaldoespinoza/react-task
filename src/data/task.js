8e3b2ffc2ab2049db2d1c1bdd2e880e1bdebfa7c
Oswaldo <joswaldoespinoza@gmail.com>
2023-09-02 19:31:18 -0500

    Primer commit

---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "Mi primera tarea",
+        descripcion: "mi primera tarea"
+    },
+    {
+        id: 1,
+        title: "Mi segunda tarea",
+        descripcion: "mi SEGUBNDA tarea"
+    },
+    {
+        id: 2,
+        title: "Mi tercera tarea",
+        descripcion: "mi tyercera tarea"
+    }
+];
